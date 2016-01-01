1d32e519d88fe23cda8f8b37a49f0c6cb3579c11
XadillaX <admin@xcoder.in>
2016-01-01 14:58:12 +0800

    create the outer files

---
+++ index.js
@@ -0,0 +1,19 @@
+/**
+ * XadillaX created at 2016-01-04 14:56:07 With ♥
+ *
+ * Copyright (c) 2016 Souche.com, all rights
+ * reserved.
+ */
+"use strict";
+
+var OCS = module.exports = require("./lib/ocs");
+
+/**
+ * create
+ * @param {String|Array} servers the servers addresses
+ * @param {Object} options the ocs options
+ * @return {OCS} the OCS wrapper
+ */
+module.exports.create = function(servers, options) {
+    return new OCS(servers, options);
+};
